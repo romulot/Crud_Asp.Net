14f7b1f3f3bd0b31ed8e88864127b0ed3f5d705a
Ramulo <romulot.almeida@gmail.com>
2017-11-16 08:10:18 -0200

    Add Projeto
    Adicionando pasta principal do projeto.

---
+++ teste/teste/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(teste.Startup))]
+namespace teste
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
